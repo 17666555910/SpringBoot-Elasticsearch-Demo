6285e03c952cb8011c6b069052580ac9629dffd4
lunzi <757552030@qq.com>
2020-02-22 15:57:44 +0800

    创建工程

---
+++ src/main/java/com/xinghua/elasticsearchservice/ElasticsearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xinghua.elasticsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xinghua/elasticsearchservice/ElasticsearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinghua.elasticsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
